nataliakokoromyti/OpenSTA

commit f18bfbbc4c1bc463df2cb7e30b91234578cfb686

source set info script
Signed-off-by: James Cherry <cherry@parallaxsw.com>

sdc/Sdc.tcl
@@ -42,7 +42,13 @@ proc_redirect read_sdc {
   check_argc_eq1 "read_sdc" $args
   set echo [info exists flags(-echo)]
   set filename [file nativename [lindex $args 0]]
-  source_ $filename $echo 0
+  set prev_filename [info script]
+  try {
+    info script $filename
+    source_ $filename $echo $verbose
+  } finally {
+    info script $prev_filename
+  }
 }
 
 ################################################################
@@ -70,7 +76,13 @@ proc_redirect source {
   set echo [info exists flags(-echo)]
   set verbose [info exists flags(-verbose)]
   set filename [file nativename [lindex $args 0]]
-  source_ $filename $echo $verbose
+  set prev_filename [info script]
+  try {
+    info script $filename
+    source_ $filename $echo $verbose
+  } finally {
+    info script $prev_filename
+  }
 }
 
 proc source_ { filename echo verbose } {

commit 24518a2b0e8e8eeceea76716726474aae7aa4c81

read_sdc bug introduced by f18bfbbc
Signed-off-by: James Cherry <cherry@parallaxsw.com>

sdc/Sdc.tcl
@@ -45,7 +45,7 @@ proc_redirect read_sdc {
   set prev_filename [info script]
   try {
     info script $filename
-    source_ $filename $echo $verbose
+    source_ $filename $echo 0
   } finally {
     info script $prev_filename
   }